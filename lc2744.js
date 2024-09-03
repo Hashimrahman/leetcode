46774cb8964861516658d0c44f8a5e5862b2a00e
Hashimrahman <hashimrahmanrh@gmail.com>
2024-09-03 22:35:12 +0530

    Problem : Find Maximum Number of String Pairs

---
+++ lc2744.js
@@ -0,0 +1,11 @@
+var maximumNumberOfStringPairs = function (words) {
+    let count = 0;
+    for (let i = 0; i < words.length; i++) {
+        for (let j = i + 1; j < words.length; j++) {
+            if (words[i] == (words[j].split('').reverse().join(''))) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
