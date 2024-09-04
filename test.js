02050307556bb249a87032a323e26fcbc8f600d1
Hashimrahman <hashimrahmanrh@gmail.com>
2024-09-04 22:18:15 +0530

    Problem : Count Odd Numbers in an Interval Range

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
-let obj = {1:'hi',2:'hello'};
-let arr = JSON.parse(obj);
-console.log(arr);
+// let obj = {1:'hi',2:'hello'};
+// let arr = JSON.parse(obj);
+// console.log(arr);
+
+
+function secondLargest(arr){
+    let largest = arr[0];
+    let secondLargest = null;
+    for(let i = 1; i< arr.length;i++){
+        if(arr[i] > largest){
+            secondLargest = largest;
+            largest = arr[i];
+        }
+    }
+
+    return secondLargest;
+}
+
+console.log(secondLargest([1,1,1,1]));
