1bee2eec42217395414acddf03de89bf35f77401
Hashimrahman <hashimrahmanrh@gmail.com>
2024-09-05 22:09:45 +0530

    Problem : Create Hello World Function

--- test.js
+++ test.js
@@ -3,17 +3,32 @@
 // console.log(arr);
 
 
-function secondLargest(arr){
+function secondLargest(arr) {
     let largest = arr[0];
     let secondLargest = null;
-    for(let i = 1; i< arr.length;i++){
-        if(arr[i] > largest){
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
             secondLargest = largest;
             largest = arr[i];
         }
+        else if (secondLargest < arr[i]) {
+            secondLargest = arr[i]
+        }
     }
-
     return secondLargest;
 }
 
-console.log(secondLargest([1,1,1,1]));
+console.log(secondLargest([1, 4, 8, 2, 9]));
+// let arr = [1, 12, 8, 2, 9,6,11];
+// let largest = arr[0];
+// let secondLargest = null;
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > largest) {
+//         secondLargest = largest;
+//         largest = arr[i];
+//     }
+//     else if(secondLargest<arr[i]){
+//         secondLargest = arr[i]
+//     }
+// }
+// console.log(secondLargest);
