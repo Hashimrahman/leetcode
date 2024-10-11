c8bb1dd3d1fc7f8394429b339172e7335f508314
Hashimrahman <hashimrahmanrh@gmail.com>
2024-10-11 23:09:24 +0530

    Problem : The Number of the Smallest Unoccupied Chair

---
+++ lc1942.js
@@ -0,0 +1,27 @@
+var smallestChair = function(times, targetFriend) {
+    for (let i=0; i<times.length; i++) {
+        times[i].push(i);
+    }
+
+    times.sort((a, b) => a[0] - b[0]);
+    let seats = [];
+
+    for (let friend of times) {
+        let i = 0;
+        if (seats.length == 0) seats.push(friend[1]);
+        else {
+            while (seats[i] > friend[0]) {
+                i++;
+            }
+            if (i < seats.length) {
+                seats[i] = friend[1];
+            } else {
+                seats.push(friend[1]);
+            }
+        }
+        if (friend[2] == targetFriend) {
+            return i;
+        }
+    }
+    return 0;
+};
